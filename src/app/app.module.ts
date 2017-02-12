04a587ef4f9e0eb041d419f980feb5839b81478e
it-rookie <jwen@fly2east.com>
2017-02-12 16:37:34 -0600

    Changed Home page to Tools and added basic controls without backing data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+import { ToolsPage } from '../pages/tools/tools';
 import { TabsPage } from '../pages/tabs/tabs';
 
 @NgModule({
@@ -11,7 +11,7 @@ import { TabsPage } from '../pages/tabs/tabs';
     MyApp,
     AboutPage,
     ContactPage,
-    HomePage,
+    ToolsPage,
     TabsPage
   ],
   imports: [
@@ -22,7 +22,7 @@ import { TabsPage } from '../pages/tabs/tabs';
     MyApp,
     AboutPage,
     ContactPage,
-    HomePage,
+    ToolsPage,
     TabsPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
---
+++ src/pages/tools/tools.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'tools',
+  templateUrl: 'tools.html'
+})
+export class ToolsPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
