559c0a7b0f283fed7054458ba0d4142878f49184
Lihe Chen <lihe.chen@gmail.com>
2017-05-22 20:55:15 -0500

    works!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,11 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { firebaseConfig }       from './app.config';
+
+
 
 @NgModule({
   declarations: [
@@ -21,7 +26,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -34,7 +41,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
   ]
 })
-export class AppModule {}
+export class AppModule { }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,40 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  private tools = [{
-        "id": "1",
-        "name": "Cub Cadet XT1 Enduro Series LT 42 in. 18 HP Kohler Hydrostatic Gas Front-Engine Riding Mower" ,
-        "image": "http://www.homedepot.com/catalog/productImages/400_compressed/73/7378ee12-b915-4972-ad44-7dd7d6fef7b4_400_compressed.jpg"
-        },
-        {
-        "id": "2",
-        "name": "Ryobi ONE+ 18-Volt Lithium-Ion Ultimate Combo Kit (6-Tool)" ,
-        "image": "http://www.homedepot.com/catalog/productImages/400_compressed/59/5967d2f1-ac37-4f96-b7a4-79f93b92e193_400_compressed.jpg"
-        },
-        {
-        "id": "3",
-        "name": "Ryobi 40-Volt 16 in. Lithium-Ion Cordless Walk Behind Battery Push Mower with 4.0 Ah Battery" ,
-        "image": "http://www.homedepot.com/catalog/productImages/400_compressed/7e/7e14da71-bf1b-487e-abf7-d957bb786fbb_400_compressed.jpg"
-        },
-        {
-        "id": "4",
-        "name": "EGO 110 mph 530 CFM Variable-Speed Turbo 56-Volt Lithium-ion Cordless Electric Blower" ,
-        "image": "http://www.homedepot.com/catalog/productImages/400_compressed/4e/4e0b1d79-25e6-4457-8dde-93229f2720cd_400_compressed.jpg"
-        },
-        {
-        "id": "5",
-        "name": "Nexgrill 4-Burner Propane Gas Grill in Stainless Steel with Side Burner" ,
-        "image": "http://www.homedepot.com/catalog/productImages/400_compressed/c5/c51ae7dc-ecae-41d5-bf33-af434624833d_400_compressed.jpg"
-        }
-    ]
-  constructor(public navCtrl: NavController) {
+  private tools: FirebaseListObservable<any[]>;
 
+  constructor(public navCtrl: NavController, db: AngularFireDatabase) {
+    this.tools = db.list('/tools');
   }
 
 }
