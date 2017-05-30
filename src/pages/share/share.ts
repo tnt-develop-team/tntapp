8d03f4c99069c8d4137ee2ac462ad35686ecc175
Lihe Chen <lihe.chen@gmail.com>
2017-05-30 11:12:59 -0500

    add marker

--- src/pages/share/share.ts
+++ src/pages/share/share.ts
@@ -48,4 +48,28 @@ export class SharePage {
       console.log(err);
     });
   }
+
+  addMarker() {
+    let marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: this.map.getCenter()
+    });
+
+    let content = "<h4>Information!</h4>";
+
+    this.addInfoWindow(marker, content);
+  }
+
+  addInfoWindow(marker, content) {
+
+    let infoWindow = new google.maps.InfoWindow({
+      content: content
+    });
+
+    google.maps.event.addListener(marker, 'click', () => {
+      infoWindow.open(this.map, marker);
+    });
+
+  }
 }
